1f7a5181d6268edfc290d75a3d628b1043cbaa6e
bualter <walterjgonzalez@gmail.com>
2018-11-13 20:39:23 -0300

    Mejoraron las preguntas frecuentes

--- resources/views/preguntasFrecuentes.blade.php
+++ resources/views/preguntasFrecuentes.blade.php
@@ -1,1 +1,21 @@
-Aca van las preguntas frecuentes
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                  <h2> Preguntas Frecuentes. </h2>
+                </div>
+
+                <div class="card-body">
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
